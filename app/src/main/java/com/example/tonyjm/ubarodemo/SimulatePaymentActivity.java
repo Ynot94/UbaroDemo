91a34a151717fca9259062e86dc8b4ee4f75b612
Tony Jaime M <tjmp94@gmail.com>
2016-09-18 12:05:26 -0500

    Added rounded buttons and functionality

---
+++ app/src/main/java/com/example/tonyjm/ubarodemo/SimulatePaymentActivity.java
@@ -0,0 +1,13 @@
+package com.example.tonyjm.ubarodemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimulatePaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simulate_payment);
+    }
+}
